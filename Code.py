790e32faf8da8dc4d1e3bec82d9df3f97dd9da84
marya <maryadukmak@gmail.com>
2020-02-12 13:45:18 +0100

    opdracht 1

---
+++ Code.py
@@ -0,0 +1,13 @@
+
+import random
+n1= input("First color?")
+n2= input("Second color?")
+n3= input("Third color?")
+n4= input("Forth color?")
+
+n1_random= random.sample(["wit", "rood","groen", "geel", "blauw", "zwart"],1)
+n2_random= random.sample(["wit", "rood","groen", "geel", "blauw", "zwart"],1)
+n3_random= random.sample(["wit", "rood","groen", "geel", "blauw", "zwart"],1)
+n4_random= random.sample(["wit", "rood","groen", "geel", "blauw", "zwart"],1)
+
+for i in
