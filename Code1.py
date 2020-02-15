b480b3b78ee63ebbd087e851f93b8409b1b85ab1
marya <maryadukmak@gmail.com>
2020-02-16 00:53:07 +0100

    opdracht 1

--- Code1.py
+++ Code1.py
@@ -18,22 +18,22 @@ def keuze_m():
 
 def start():
     print("---Mastermind game---")
-    print("Kies uit de volgende kleuren: wit, rood, groen, geel, blauw, zwart en paars. ")
+    print("Kies uit de volgende kleuren: wit, rood, groen, geel, blauw en zwart. ")
     code_speler = []
     while len(code_speler) < 4:
-        code_input=(input("Guess de kleuren : ")).lower()
+        code_input=(input("Guess de kleuren: ")).lower()
         if code_input=="stop":
             print("Het spel is gestopt.")
             exit()
         else:
             code_speler.append(code_input)
     #code_speler = ["blauw", "zwart", "groen","rood"]
-    code = secret_code()
+    code = Random_secret_code()
     count_aantal_pogingen =  0
     feedback(code, code_speler, count_aantal_pogingen)
 
-def secret_code():
-    #kleuren= ["wit", "rood","groen", "geel", "blauw", "zwart", "paars"]
+def Random_secret_code():
+    #kleuren= ["wit", "rood","groen", "geel", "blauw", "zwart"]
     #code= random.sample(kleuren, 4 )
     #print(code)
     code = ["blauw", "zwart", "groen","rood"]
@@ -64,11 +64,16 @@ def  vergelijking (code, code_speler):
 
                 else:
                      continue
+
             return klopt_kleuren, klopt_positie
 
 def feedback(code, code_speler, count_aantal_pogingen):
     count_aantal_pogingen += 1
+
     klopt_kleuren, klopt_positie = vergelijking(code, code_speler)
+    if count_aantal_pogingen==10:
+        print("Je hebt de maximale aantal pogingen bereikt. Probeert het opnieuw")
+        exit()
     if klopt_positie == 4:
          print("Goed gedaan! Je bent een Mastermind!")
          print("Je hebt {} pogingen in totaal.".format(count_aantal_pogingen))
@@ -87,7 +92,7 @@ def feedback(code, code_speler, count_aantal_pogingen):
 
 def computer_guess():
     print("Geef me 4 kleuren om te raden," '\n'
-          "Kies uit de volgende kleuren: wit, rood, groen, geel, blauw, zwart en paars. ")
+          "Kies uit de volgende kleuren: wit, rood, groen, geel, blauw en zwart . ")
     print('\r')
     secret_code = []
     while len(secret_code) < 4:
@@ -97,28 +102,36 @@ def computer_guess():
             exit()
         else:
             secret_code.append(code_input)
-        return secret_code
-    print("Please type: 1 als mijn gok fout is" '\n'
-          "           : 2 als mijn gok goed is")
 
-    humanFeedback = input("Dus had ik het goed?")
-    if 1 < humanFeedback > 2:
-        print("Voer een geldig keuze in:")
+    print(secret_code)
+    return secret_code
+
+    #print("Please type: 1 als mijn gok fout is" '\n'
+          #"           : 2 als mijn gok goed is")
+
+    #humanFeedback = input("Dus had ik het goed?")
+
+def vergelijk_codes(gok1, secret_code_input):
+    gok1= Random_secret_code()
+    secret_code_input= computer_guess()
+    vergelijking()
+
 
 def mogelijk_gok():
-    kleuren= ["wit", "rood","groen", "geel", "blauw", "zwart", "paars"]
+    kleuren= ["wit", "rood","groen", "geel", "blauw", "zwart"]
     alle_mogelijkheden= []
     for i in kleuren:
         for k in kleuren:
             for c in kleuren:
                 for g in kleuren:
-                    alle_mogelijkheden.append([i,k,c,g])
-    print(alle_mogelijkheden)
+                    alle_mogelijkheden.append([i , k ,c , g ])
+
     return alle_mogelijkheden
+
 
     keuze_m()
 
-
+mogelijk_gok()
 
 
 keuze_m()
--- Code2 .py
+++ Code2 .py
@@ -1,20 +1,73 @@
 import Code1 as fl
 
 def computer_guess():
-   print("Geef me 4 kleuren om te raden," '\n'
-         "Kies uit de volgende kleuren: wit, rood, groen, geel, blauw, zwart en paars. ")
-   secret_code= []
-   while len(secret_code) < 4:
-       code_input = (input("Geef me de kleuren : ")).lower()
-       if code_input == "stop":
-           exit()
-       else:
-           secret_code.append(code_input)
-       print(secret_code)
-   print("Please type: 1 als mijn gok fout is"'\n'
-         "           : 2 als mijn gok goed is")
-   humanFeedback= input("Dus had ik het goed?")
+    print("Geef me 4 kleuren om te raden," '\n'
+          "Kies uit de volgende kleuren: wit, rood, groen, geel, blauw en zwart . ")
+    print('\r')
+    secret_code = []
+    while len(secret_code) < 4:
+        code_input = (input("Geef me de kleuren: ")).lower()
+        if code_input == "stop":
+            print("Het spel is gestopt.")
+            exit()
+        else:
+            secret_code.append(code_input)
 
-   fl.keuze_m()
-   fl.vergelijking( )
-fl.keuze_m()
+    print(secret_code)
+    return secret_code
+
+    #print("Please type: 1 als mijn gok fout is" '\n'
+          #"           : 2 als mijn gok goed is")
+
+    #humanFeedback = input("Dus had ik het goed?")
+def  vergelijking (secret_code_input, gok1):
+    if (gok1==secret_code_input):
+        return 0,4
+    else:
+        while(gok1 != secret_code_input):
+            #klopt kleuren= wit
+            #klopt positie= zwart
+            klopt_kleuren = 0
+            klopt_positie= 0
+            code_speler_list=[]
+            code_list=[]
+            #wit
+            for j in range(0, len(secret_code_input)):
+                if (secret_code_input[j] == gok1[j]):
+                    klopt_positie+=1
+                else:
+                    code_speler_list.append(gok1[j])
+                    code_list.append(secret_code_input[j])
+            #zwart
+            for j in range(0, len(code_list)):
+                if (code_list[j] in code_speler_list):
+                     klopt_kleuren+=1
+
+                else:
+                     continue
+
+            return klopt_kleuren, klopt_positie
+def vergelijk_codes(gok1, secret_code_input):
+    gok1= fl.Random_secret_code()
+    secret_code_input= computer_guess()
+    return gok1
+    return secret_code_input
+
+
+
+
+def mogelijk_gok():
+    kleuren= ["wit", "rood","groen", "geel", "blauw", "zwart"]
+    alle_mogelijkheden= []
+    for i in kleuren:
+        for k in kleuren:
+            for c in kleuren:
+                for g in kleuren:
+                    alle_mogelijkheden.append([i , k ,c , g ])
+
+    return alle_mogelijkheden
+
+
+    keuze_m()
+
+mogelijk_gok()
